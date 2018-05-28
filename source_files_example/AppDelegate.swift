2f5c736c21ec5024f86a9ccf764090dc2eafff56
Daniel Alm <Daniel.Alm@ForumD.net>
2018-05-28 10:37:25 +0200

    Initial Commit

---
+++ source_files_example/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  source_files_example
+//
+//  Created by Daniel Alm on 28.05.18.
+//  Copyright © 2018 Daniel Alm. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+	@IBOutlet weak var window: NSWindow!
+
+
+	func applicationDidFinishLaunching(_ aNotification: Notification) {
+		// Insert code here to initialize your application
+	}
+
+	func applicationWillTerminate(_ aNotification: Notification) {
+		// Insert code here to tear down your application
+	}
+
+
+}
+
